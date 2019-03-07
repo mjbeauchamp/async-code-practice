9327ecb49a377a3281e4369c30d03fcb59fc0387
Monica Beauchamp <monica.j.beauchamp@gmail.com>
2019-03-07 11:46:33 -0700

    Basic async callbacks working

---
+++ index.js
@@ -0,0 +1,23 @@
+//Handling async code using callbacks
+
+// function mission(callback){
+//     let missionStatus = "incomplete"
+//     setTimeout(() => {
+//         missionStatus = 'complete'
+//         callback(missionStatus)
+//     }, 2000)
+//     console.log(`The mission is in progress... Status: ${missionStatus}`)
+// }
+
+
+
+// function missionUpdate(status){
+//     console.log(`The mission is complete! Status: ${status}`)
+// }
+
+// mission(missionUpdate)
+
+
+
+//Handling async code using Promises
+
